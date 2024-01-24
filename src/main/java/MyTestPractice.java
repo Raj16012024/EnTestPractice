a04b4b7051be285d6e5b138278a73b4639239a39
Raj16012024 <mrsrajee@yahoo.co.uk>
2024-01-24 23:17:09 +0000

    Second Commit

--- src/main/java/MyTestPractice.java
+++ src/main/java/MyTestPractice.java
@@ -1,5 +1,5 @@
 public class MyTestPractice {
     public static void main (String [] args){
-        System.out.println("Hello and Welcome");
+        System.out.println("Hello and Welcome to my Maven practice class");
     }
 }
